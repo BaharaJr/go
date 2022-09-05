82fef1508e6642ceb6abf2ec7dfbd67a681a2b90
Bennett Benedict <bennybenester@gmail.com>
2022-09-05 19:39:48 +0300

    add account tests

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,27 @@
+package db
+
+import (
+	"database/sql"
+	_ "github.com/lib/pq"
+	"log"
+	"os"
+	"testing"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://postgres:postgres@localhost:5434/simple?sslmode=disable"
+)
+
+var testQueries *Queries
+
+func TestMain(m *testing.M) {
+	conn, err := sql.Open(dbDriver, dbSource)
+
+	if err != nil {
+		log.Fatal("Can not connect to db:", err)
+	}
+
+	testQueries = New(conn)
+	os.Exit(m.Run())
+}
